ff6aed94da8d2675717b13a5fec642838ae3d7ed
IT-HARISH-R <harishmass27.8@gmail.com>
2025-01-28 15:33:56 +0530

    ahange port

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const app = require("./app");
 const mongoose = require("mongoose");
 const {MONGODB_URL, PORT } = require("./utlis/config");
 
+const port = PORT || 4000
 mongoose.connect(MONGODB_URL)
     .then(() => {
         console.log("Connecting database")
-        app.listen(PORT, () => {
+        app.listen(port || 4000, () => {
             console.log("server run");
         })
     })
