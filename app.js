bf88e54d3b60644916934a8d021029bc027223c1
IT-HARISH-R <harishmass27.8@gmail.com>
2025-01-31 11:22:56 +0530

    cors origin

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        // origin: ['https://guvi-event-management-project.netlify.app'],
+        origin: ['https://guvi-event-management-project.netlify.app'],
         // origin: ['http://localhost:5173'],
-        origin: ['http://localhost:5173','https://guvi-event-management-project.netlify.app'],
+        // origin: ['http://localhost:5173','https://guvi-event-management-project.netlify.app'],
         credentials: true,
         methods: ['GET', 'POST', 'PATCH', 'DELETE' ,"PUT"],
     }
